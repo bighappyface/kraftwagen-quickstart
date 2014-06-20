a148f2efed92d657b47cf8de6b26c8ca69bbc084
davi4797 <david.porter@rackspace.com>
2014-06-20 13:06:14 -0500

    setup env vars for settings.php add kw-s and kw-b commands to build hook

--- src/cnf/settings.local.php
+++ src/cnf/settings.local.php
@@ -3,15 +3,15 @@
 $databases = array (
   'default' => array (
     'default' => array (
-      'database' => 'DATABASE_NAME',
-      'username' => 'DATABASE_USERNAME',
-      'password' => 'DATABASE_PASSWORD',
-      'host'     => 'localhost',
-      'port'     => '',
+      'database' => $_ENV['OPENSHIFT_REPO_DIR'],
+      'username' => $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
+      'password' => $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
+      'host'     => $_ENV['OPENSHIFT_MYSQL_DB_HOST'],
+      'port'     => $_ENV['OPENSHIFT_MYSQL_DB_PORT'],
       'driver'   => 'mysql',
       'prefix'   => '',
     ),
   ),
 );
 
-$drupal_hash_salt = 'SOME_REALLY_RANDOM_STRING';
+$drupal_hash_salt = $_ENV['OPENSHIFT_SECRET_TOKEN'];
